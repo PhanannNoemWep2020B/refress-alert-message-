ab12b9c68eb0120380e8fc59ec443b23e0f3927d
phanann.noem <phanann.noem@pnc.lan>
2019-12-06 20:34:10 +0700

    this is message by refress

---
+++ js/second.js
@@ -0,0 +1,43 @@
+/*var studentName = "romdul";
+var studentAge = 24;
+
+let studentProine = "Banteaymeanchey";
+let studentGrade = 12;
+
+studentProine = "Battambang";
+
+const studentId = "038";
+studentId = "PNC038";
+
+window.alert(studentProine + studentAge+ "   " + studentGrade + studentId + studentId);
+*/
+var studentName = 'Bopha';
+var age = 100;
+var avg = 30.5;
+//  boolean 
+
+var isAge = true;
+var isGrade = false;
+
+var studentAge = null;
+
+// array 
+var studentList = ("bopha","cambodian","kolab","chompey");
+
+console.log(studentList[0]);
+console.log(studentList[3]);
+
+// undifined
+var studentId;
+console.log(studentId);
+
+
+
+
+
+var numer1 = 4;
+if ( numer1 == '4'){
+    window.alert("yes");
+ } else {
+        window.alert("No")
+}
